a3a4d82344507f66128d99c82e5415543665013b
mohamed hassan <mohamedisaaq0@gmail.com>
2022-05-19 22:39:01 +0100

    added margin to the result title

--- assets/script.js
+++ assets/script.js
@@ -7,46 +7,41 @@ const searchResultsDiv = document.getElementById("search-results");
 //   console.log(searchInput.value);
 // }
 
-
 async function searchBooks(term) {
   let headers = {
-    "Content-Type": 'application/json',
-    "Authorization": '47876_2825773de299cd5e0d9827ecde49fac0'
-  }
-  const {books} = await (
-    await fetch(` https://api2.isbndb.com/books/${term}?page=1&pageSize=20&column=title
-    `,{headers:headers}
+    "Content-Type": "application/json",
+    Authorization: "47876_2825773de299cd5e0d9827ecde49fac0",
+  };
+  const { books } = await (
+    await fetch(
+      ` https://api2.isbndb.com/books/${term}?page=1&pageSize=20&column=title
+    `,
+      { headers: headers }
     )
   ).json();
   return books.map((info) => {
-    return {
-      title,
-      authors,
-      image,
-      isbn
-    } = info;
-});
+    return ({ title, authors, image, isbn } = info);
+  });
 }
 
-function fetchBook(isbn){
-  console.log(isbn)
+function fetchBook(isbn) {
+  console.log(isbn);
 }
 
 function createBookList(books, term) {
-
-    let searchTermH1 = `<h1 class="title is-1"><strong>Your results for:<span> ${term}</span></strong>...</h1><br><br>`;
-    let resultsHTML = '<div class="box">';
-    const placeholderImg = "./img/book-placeholder.png";
-    books.forEach((book) => {
-      const resultHTML = `
+  let searchTermH1 = `<h1 class="title is-1 margin-title"><strong>Your results for:<span> ${term}</span></strong>...</h1><br><br>`;
+  let resultsHTML = '<div class="box">';
+  const placeholderImg = "./img/book-placeholder.png";
+  books.forEach((book) => {
+    const resultHTML = `
                 <div class="container-result"> 
                   <article class="media container-result">
                     <div class="media-left">
                     <a href="${
                       book.infoLink
                     }" target="_blank"><img class="image is-150x115 " src="${
-        !book.image ? placeholderImg : book.image
-      }" alt="${book.title}"></a>
+      !book.image ? placeholderImg : book.image
+    }" alt="${book.title}"></a>
                     <br>
                   </div>
           <div class="media-content">
@@ -57,13 +52,13 @@ function createBookList(books, term) {
                 <div>
                 Author: <span>${book.authors}</span>
                 <br>
-                Category: <span>${book.categories}</span>
-                <br>
                 Publisher: <span>${book.publisher}</span>
                 </div>
               </p>
             </div>
-            <button id='select' class="button is-light" onclick='fetchBook(${book.isbn})'>
+            <button id='select' class="button is-light" onclick='fetchBook(${
+              book.isbn
+            })'>Select
             </button>
           </div>
           
@@ -71,20 +66,19 @@ function createBookList(books, term) {
         </br><hr>
     </div>
               `;
-      resultsHTML += resultHTML;
-    });
-    resultsHTML += "</div>";
-    searchResultsDiv.innerHTML = searchTermH1 + resultsHTML;
-    
-  }
+    resultsHTML += resultHTML;
+  });
+  resultsHTML += "</div>";
+  searchResultsDiv.innerHTML = searchTermH1 + resultsHTML;
+}
 
-  // initialiseLocalStorage();
-  // fetchBook();
-async function search(){
+// initialiseLocalStorage();
+// fetchBook();
+async function search() {
   const searchTerm = searchInput.value;
-  if(searchTerm){
-    const books =  await searchBooks(searchTerm)
-    createBookList(books, searchTerm)
+  if (searchTerm) {
+    const books = await searchBooks(searchTerm);
+    createBookList(books, searchTerm);
   }
 }
 
