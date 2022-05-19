7beec21f687016af6f624beadb05d6904b49a2f5
mohamed hassan <mohamedisaaq0@gmail.com>
2022-05-19 17:36:35 +0100

    styling result part 1

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
-const body = document.body
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
-let searchTerm = ''
+const body = document.body;
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const searchResultsDiv = document.getElementById("search-results");
+let searchTerm = "";
 
 window.addEventListener("load", () => {
   const form = document.getElementById("form");
@@ -13,46 +14,108 @@ window.addEventListener("load", () => {
 });
 
 function searchBooks() {
-    
-    searchInput.addEventListener("change", (e) => {
+  searchInput.addEventListener("change", (e) => {
     searchTerm = e.target.value;
 
-    const searchResultsDiv = document.getElementById('search-results')
-
     async function fetchingAPI() {
-        const api = await (await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`)).json();
-        const bookInfos = api.items.map(info => {
-          if(info.volumeInfo.imageLinks) {
-            const { volumeInfo: {title, categories, authors, infoLink, publisher, imageLinks:{thumbnail}}} = info
-            return {
+      const api = await (
+        await fetch(
+          `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`
+        )
+      ).json();
+      const bookInfos = api.items.map((info) => {
+        if (info.volumeInfo.imageLinks) {
+          const {
+            volumeInfo: {
               title,
               categories,
               authors,
               infoLink,
               publisher,
-              thumbnail,
-            }
-          } else {
-            const {volumeInfo: {title, categories, authors, infoLink, publisher}} = info
-            return {
-              title,
-              categories,
-              authors,
-              infoLink,
-              publisher
-            }
-          }
-        })        
-        let searchTermH1 = `<h1 class="title is-1"><strong>Your results for:<span> ${searchTerm}</span></strong>...</h1><br><br>`
-        let resultsHTML = '<section class="row">'
-        const placeholderImg = './img/book-placeholder.png'
-        bookInfos.forEach((info) => {
-          const resultHTML = `
-                <div class="">
-                    <h3 class="title is-4 title-name" id="title">${info.title}</h3>
-                    <a href="${info.infoLink}" target="_blank"><img class="image " src="${!info.thumbnail ? placeholderImg : info.thumbnail}" alt="${info.title}"></a>
+              imageLinks: { thumbnail },
+            },
+          } = info;
+          return {
+            title,
+            categories,
+            authors,
+            infoLink,
+            publisher,
+            thumbnail,
+          };
+        } else {
+          const {
+            volumeInfo: { title, categories, authors, infoLink, publisher },
+          } = info;
+          return {
+            title,
+            categories,
+            authors,
+            infoLink,
+            publisher,
+          };
+        }
+      });
+      let searchTermH1 = `<h1 class="title is-1"><strong>Your results for:<span> ${searchTerm}</span></strong>...</h1><br><br>`;
+      let resultsHTML = '<div class="box">';
+      const placeholderImg = "./img/book-placeholder.png";
+      bookInfos.forEach((info) => {
+        const resultHTML = `
+                <div class="container-result"> 
+                  <article class="media container-result">
+                    <div class="media-left">
+                    <a href="${
+                      info.infoLink
+                    }" target="_blank"><img class="image is-150x115 " src="${
+          !info.thumbnail ? placeholderImg : info.thumbnail
+        }" alt="${info.title}"></a>
                     <br>
-                    <div>
+                  </div>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>${info.title}</strong> 
+                <br>
+                <div>
+                Author: <span>${info.authors}</span>
+                <br>
+                Category: <span>${info.categories}</span>
+                <br>
+                Publisher: <span>${info.publisher}</span>
+                </div>
+              </p>
+            </div>
+            <button class="button is-light">
+            <a class="info-button" href="${
+              info.infoLink
+            }" target="_blank"><strong>Select</strong></a>
+            </button>
+            <button class="button is-light">
+            <a class="info-button" href="${
+              info.infoLink
+            }" target="_blank"><strong>Select</strong></a>
+            </button>
+          </div>
+          
+        </article>
+        </br><hr>
+    </div>
+              `;
+        resultsHTML += resultHTML;
+      });
+      resultsHTML += "</div>";
+      searchResultsDiv.innerHTML = searchTermH1 + resultsHTML;
+    }
+    // searchInput.value = ""
+    fetchingAPI();
+  });
+}
+
+const initialiseLocalStorage = () => {
+  localStorage.setItem("Previous Search", searchInput.value);
+};
+
+/*{ <div>
                         <h4>Author: <span>${info.authors}</span></h4>
                         <h4>Category: <span>${info.categories}</span></h4>
                         <h4>Publisher: <span>${info.publisher}</span></h4>
@@ -63,19 +126,4 @@ function searchBooks() {
                         <a class="info-button" href="${info.infoLink}" target="_blank"><strong>Select</strong></a>
                         </button>
                     </div>
-                    <br>
-                <div>
-              `
-              resultsHTML += resultHTML
-
-       }) 
-       resultsHTML += '</section>'
-       searchResultsDiv.innerHTML = searchTermH1 + resultsHTML
-    }
-    searchInput.value = ""
-    fetchingAPI();
-    });
-}
-
-
-
+                    <br> }*/
