bd79c3e376d1c43ea8bf02f319e9692f9fc1ab8e
mohamed hassan <mohamedisaaq0@gmail.com>
2022-05-22 02:52:54 +0100

    htmls dynamically created for saved books section

--- assets/savedBookscript.js
+++ assets/savedBookscript.js
@@ -1,17 +1,43 @@
 let booklist = "";
+const removeButton = document.getElementById("remove-button");
 
-const searchResultsDiv = document.getElementById("search-results");
+const savedBooks = document.getElementById("saved-books");
+keys = Object.keys(localStorage);
 
-const retiveDatefromls = () => {
-  for (var i = 0; i < localStorage.length; i++) {
-    var key = localStorage.key(i);
-    var value = localStorage.getItem(key);
-    booklist = JSON.parse(value);
-  }
-};
-const renderBooklist = () => {
-  console.log(booklist);
-};
+function bookStorage() {
+  keys.forEach((key) => {
+    const book = localStorage.getItem(key);
+    const stringifyBook = JSON.parse(book);
+    console.log(stringifyBook);
+    booklist = `
+    <div class="box">
+      <article class="media">
+        <div class="media-left">
+          <img
+            class="image is-150x115"
+            src="${stringifyBook.thumbnail}"
+            alt="${stringifyBook.title}"
+          />
+          <br />
+        </div>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <strong>${stringifyBook.title}</strong>
+              <br />
+              Author: <span>${stringifyBook.authors}</span>
+              <br />
+              Average ratings: <span>${stringifyBook.averageRating}</span>/5
+            </p>
+          </div>
+          <button id="remove-button" class="button is-light" onclick=>Remove</button>
+        </div>
+      </article>
+      </div>
+    `;
+    console.log(booklist);
+  });
+  savedBooks.innerHTML = booklist;
+}
 
-retiveDatefromls();
-renderBooklist();
+bookStorage();
